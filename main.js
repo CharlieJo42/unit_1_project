fd8f1b82194bf1471523f8c8621130536d6a0170
Charlie <charliejames1652@gmail.com>
2020-07-22 19:21:55 -0400

    changing focus css to array in js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // function popUp(message) {
 //     alert(message);
 // }
+
+var colorArray = ["Red", "Red1", "Orange", "Orange1", 
+"Yellow", "Yellow1", "Green", "Green1", "Blue", "Blue1", 
+"Purple", "Purple1", "Pink", "Pink1", "Black", "Black1",
+"Brown", "Brown1"];
+
+//code on line 11 taken from stackoverflow//
+const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+console.log("random color =>", randomColor);
