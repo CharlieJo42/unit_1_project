cac8923b010fc6975f4f65d47f396f7066135bb9
Charlie <charliejames1652@gmail.com>
2020-07-23 18:07:10 -0400

    updated clickEvent

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 // function popUp(message) {
 //     alert(message);
 // }
-
-var colorArray = ["Red", "Red", "Orange", "Orange", 
-"Yellow", "Yellow", "Green", "Green", "Blue", "Blue", 
-"Purple", "Purple", "Pink", "Pink", "Black", "Black",
-"Brown", "Brown"];
+//                  0      1        2         3       4           5         6        7       8        9      10        11        12     13        14      15      16      17            //
+var colorArray = ["Red", "Red", "Orange", "Orange", "Yellow", "Yellow", "Green", "Green", "Blue", "Blue", "Purple", "Purple", "Pink", "Pink", "Black", "Black","Brown", "Brown"];
 
 // //code on line 11 taken from stackoverflow//
 // var randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
